4ac9c6d13f0eca693b2c4dde8ef8df37743a1a3c
Phi Nguyen <nguyenlehuuphi92@gmail.com>
2025-02-20 07:43:26 +0700

    update Lab 3_3, 3_4

--- labs/lab-3/Lab_3_3.js
+++ labs/lab-3/Lab_3_3.js
@@ -4,4 +4,17 @@
     Expected output: [1, 12, 16, 28, 34]
  */
 
-    
+// let myArray = [12, 34, 1, 16, 28];
+let myArray = [12, 34, 19, 16, 1, 25];
+
+for (let i = 0; i < myArray.length; i++) {
+    for (let j = i + 1; j < myArray.length; j++) {
+        if (myArray[j] < myArray[i]) {
+            let temp  = myArray[i];
+            myArray[i] = myArray[j];
+            myArray[j] = temp;
+        }
+    }
+}
+
+console.log(myArray);
---
+++ labs/lab-3/Lab_3_4.js
@@ -0,0 +1,28 @@
+/**
+ * Merge 2 SORTED integer array into one SORTED array
+ * Array 01: [1, 12, 16, 28, 34]
+ * Array 02: [1, 13, 16, 27, 99]
+
+    Expected output: [1, 1, 12, 13, 16, 16, 27, 28, 34, 99]
+ */
+
+
+let myArray_01 = [1, 12, 16, 28, 34];
+let myArray_02 = [1, 13, 16, 27, 99];
+
+let mergedArray = myArray_01.concat(myArray_02);
+
+console.log("Array after merged:", `[${mergedArray},]`);
+console.log(`------------------------`);
+
+for (let i = 0; i < mergedArray.length; i++) {
+    for (let j = i + 1; j < mergedArray.length; j++) {
+        if (mergedArray[j] < mergedArray[i]) {
+            let temp  = mergedArray[i];
+            mergedArray[i] = mergedArray[j];
+            mergedArray[j] = temp;
+        }
+    }
+}
+
+console.log(`Array after sorted: [${mergedArray},]`);
